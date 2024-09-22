1698c095e6da4abf561499c21a9b7cbef32cf7a3
HomeDesktop <nr8122@gmail.com>
2024-09-22 16:32:16 +0900

    +tailwind autofixer postcss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
-    
+    <div class="text-center p-4 bg-blue-500 text-white">
+        <h1 class="text-3xl font-bold">Hello Tailwind with Vue 3!</h1>
+    </div>
 </template>
 
 <script setup>
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
