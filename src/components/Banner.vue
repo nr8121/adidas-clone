be70d5c1d7b5a6ffb43402e771fad255c9c9bd83
HomeDesktop <nr8122@gmail.com>
2024-10-05 21:54:40 +0900

    layout fix

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -2,16 +2,16 @@
     <section>
         <div class="absolute flex h-full ml-16">
             <div class="flex flex-col justify-end pb-14">
-                <div class="mb-1 bg-white">
-                    <span class="font-semibold bg-white text-2xl">UNLOCK THE BEST OF ADICLUB</span>
+                <div class="mb-2 bg-white">
+                    <span class="font-semibold bg-white text-2xl px-1 py-[2px]">UNLOCK THE BEST OF ADICLUB</span>
                 </div>
                 <div class="mb-4">
-                    <span class="bg-white">열어봐, 너의 모든 혜택!</span>
+                    <span class="bg-white px-1 py-[2px]">열어봐, 너의 모든 혜택!</span>
                 </div>
                 <div>
                     <button class="bg-white p-3 relative
                         hover:before:border-slate-400
-                        before:z-[-1] before:content-[''] before:absolute before:border before:w-full before:h-full before:top-[5px] before:left-[5px]"><p class="text-md font-bold">자세히 보기</p></button>
+                        before:z-[-1] before:content-[''] before:absolute before:border before:w-full before:h-full before:top-[4px] before:left-[4px]"><p class="text-md font-bold">자세히 보기</p></button>
                 </div>
             </div>
         </div>
