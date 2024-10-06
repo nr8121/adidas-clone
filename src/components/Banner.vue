fdaf68d61fddaf0e46827d65207b37e1e49c6412
HomeDesktop <nr8122@gmail.com>
2024-10-06 12:04:36 +0900

    Banner 끝

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-    <section>
+    <section class="relative">
         <div class="absolute flex h-full ml-16">
             <div class="flex flex-col justify-end pb-14">
                 <div class="mb-2 bg-white">
@@ -9,30 +9,27 @@
                     <span class="bg-white px-1 py-[2px]">열어봐, 너의 모든 혜택!</span>
                 </div>
                 <div>
-                    <button class="bg-white p-3 relative
+                    <button
+                        class="bg-white p-3 relative
                         hover:before:border-slate-400
-                        before:z-[-1] before:content-[''] before:absolute before:border before:w-full before:h-full before:top-[4px] before:left-[4px]"><p class="text-md font-bold">자세히 보기</p></button>
+                        before:z-[-1] before:content-[''] before:absolute before:border before:w-full before:h-full before:top-[4px] before:left-[4px]">
+                        <span class="text-md font-bold">자세히 보기</span>
+                        <IconArrowLong class="w-10 h-10 inline-block" />
+                    </button>
                 </div>
             </div>
         </div>
         <picture class="relative z-[-2]">
-            <source
-                srcset="/images/banner-lg.avif"
-                media="(min-width: 960px)" width="2880" height="1280">
-            <source
-                srcset="/images/banner-md.avif"
-                media="(min-width: 768px)" width="1600" height="1600">
-            <source
-                srcset="/images/banner-sm.avif"
-                media="(max-width: 767px)" width="750" height="964"><img
-                src="/images/banner-df.avif"
-                alt="banner" loading="eager" fetchpriority="high">
+            <source srcset="/images/banner-lg.avif" media="(min-width: 960px)" width="2880" height="1280">
+            <source srcset="/images/banner-md.avif" media="(min-width: 768px)" width="1600" height="1600">
+            <source srcset="/images/banner-sm.avif" media="(max-width: 767px)" width="750" height="964"><img
+                src="/images/banner-df.avif" alt="banner" loading="eager" fetchpriority="high">
         </picture>
     </section>
 </template>
 
 <script setup>
-
+import IconArrowLong from "@/components/icons/IconArrowLong.vue"
 </script>
 
 <style scoped></style>
---
+++ src/components/icons/IconArrowLong.vue
@@ -0,0 +1,10 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg">
+        <symbol fill="none" viewBox="0 0 32 32" id="arrow-long" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd"
+                d="m23 11-.707.707 3.793 3.793H4v1h22.086l-3.793 3.793L23 21l5-5-5-5Z" fill="currentColor"></path>
+        </symbol>
+        <use href="#arrow-long"></use>
+        <title>Arrow long</title>
+    </svg>
+</template>
