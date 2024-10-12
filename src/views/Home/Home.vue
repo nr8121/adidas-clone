3601b1c2439c5928b03ed2c6f580dbe09b8f546a
HomeDesktop <nr8122@gmail.com>
2024-10-12 18:42:01 +0900

    슬라이드 컨텐츠 추가.

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,12 +1,15 @@
 <template>
     <Header :notification="''"></Header>
     <Banner/>
+    <XSlideContent/>
+    
 </template>
 
 <script setup>
 import { ref } from 'vue';
 import Banner from '@/components/Banner.vue';
 import Header from '@/components/Header.vue';
+import XSlideContent from '@/components/XSlideContent.vue';
 </script>
 
 <style scoped></style>
