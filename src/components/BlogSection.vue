ae3da4c5b155a4a80430f964667c89b583d5c3d1
HomeDesktop <nr8122@gmail.com>
2024-10-14 23:11:18 +0900

    빠진항목 추가

---
+++ src/components/BlogSection.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="my-[2rem]">
+        <div class="mb-4 pl-[3rem]">
+            <p class="text-2xl font-semibold">아디다스 블로그</p>
+        </div>
+        <div class="flex px-[3rem]">
+            <div class="basis-[22%] pr-[1rem]">
+                <picture>
+                    <source
+                        srcset="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_d_cd3777a599.jpg"
+                        media="(min-width: 960px)" width="1600" height="533">
+                    <source
+                        srcset="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_t_50c2d4e412.jpg"
+                        media="(min-width: 768px)" width="1200" height="1600">
+                    <source
+                        srcset="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_m_9ccc4710ac.jpg"
+                        media="(max-width: 767px)" width="1200" height="1420">
+                        <img
+                        class="w-full h-full object-cover"
+                        src="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_d_cd3777a599.jpg"
+                        alt="kr-why_ultraboosts_are_great_for_running-running-blogs-head-asset" loading="lazy"
+                        fetchpriority="auto">
+                </picture>
+                <div class="relative p-2 ml-5 mt-[-2rem] bg-white">
+                    <span class="text-xl"><strong>러너라면 울트라부스트를 신어야 하는 이유</strong></span>
+                    <p class="line-clamp-4 text-lg">울트라부스트는 과연 좋은 러닝화일까? 물론입니다. 트레일 러너부터 풀 마라톤을 준비하는 러너까지, 아디다스 울트라부스트5가 왜 좋은 러닝화인지 그 이유를 알려드리려 합니다.</p>
+                </div>
+            </div>
+            <div class="basis-[22%] pr-[1rem]">
+                <picture>
+                    <source
+                        srcset="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_d_cd3777a599.jpg"
+                        media="(min-width: 960px)" width="1600" height="533">
+                    <source
+                        srcset="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_t_50c2d4e412.jpg"
+                        media="(min-width: 768px)" width="1200" height="1600">
+                    <source
+                        srcset="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_m_9ccc4710ac.jpg"
+                        media="(max-width: 767px)" width="1200" height="1420">
+                        <img
+                        class="w-full h-full object-cover"
+                        src="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_d_cd3777a599.jpg"
+                        alt="kr-why_ultraboosts_are_great_for_running-running-blogs-head-asset" loading="lazy"
+                        fetchpriority="auto">
+                </picture>
+                <div class="relative p-2 ml-5 mt-[-2rem] bg-white">
+                    <span class="text-xl"><strong>러너라면 울트라부스트를 신어야 하는 이유</strong></span>
+                    <p class="line-clamp-4 text-lg">울트라부스트는 과연 좋은 러닝화일까? 물론입니다. 트레일 러너부터 풀 마라톤을 준비하는 러너까지, 아디다스 울트라부스트5가 왜 좋은 러닝화인지 그 이유를 알려드리려 합니다.</p>
+                </div>
+            </div>
+            <div class="basis-[22%] pr-[1rem]">
+                <picture>
+                    <source
+                        srcset="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_d_cd3777a599.jpg"
+                        media="(min-width: 960px)" width="1600" height="533">
+                    <source
+                        srcset="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_t_50c2d4e412.jpg"
+                        media="(min-width: 768px)" width="1200" height="1600">
+                    <source
+                        srcset="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_m_9ccc4710ac.jpg"
+                        media="(max-width: 767px)" width="1200" height="1420">
+                        <img
+                        class="w-full h-full object-cover"
+                        src="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_d_cd3777a599.jpg"
+                        alt="kr-why_ultraboosts_are_great_for_running-running-blogs-head-asset" loading="lazy"
+                        fetchpriority="auto">
+                </picture>
+                <div class="relative p-2 ml-5 mt-[-2rem] bg-white">
+                    <span class="text-xl"><strong>러너라면 울트라부스트를 신어야 하는 이유</strong></span>
+                    <p class="line-clamp-4 text-lg">울트라부스트는 과연 좋은 러닝화일까? 물론입니다. 트레일 러너부터 풀 마라톤을 준비하는 러너까지, 아디다스 울트라부스트5가 왜 좋은 러닝화인지 그 이유를 알려드리려 합니다.</p>
+                </div>
+            </div>
+            <div class="basis-[22%] pr-[1rem]">
+                <picture>
+                    <source
+                        srcset="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_d_cd3777a599.jpg"
+                        media="(min-width: 960px)" width="1600" height="533">
+                    <source
+                        srcset="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_t_50c2d4e412.jpg"
+                        media="(min-width: 768px)" width="1200" height="1600">
+                    <source
+                        srcset="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_m_9ccc4710ac.jpg"
+                        media="(max-width: 767px)" width="1200" height="1420">
+                        <img
+                        class="w-full h-full object-cover"
+                        src="https://brand.assets.adidas.com/image/upload/running_fw24_ub5_global_sustain_blog_great_running_shoe_masthead_primary_d_cd3777a599.jpg"
+                        alt="kr-why_ultraboosts_are_great_for_running-running-blogs-head-asset" loading="lazy"
+                        fetchpriority="auto">
+                </picture>
+                <div class="relative p-2 ml-5 mt-[-2rem] bg-white">
+                    <span class="text-xl"><strong>러너라면 울트라부스트를 신어야 하는 이유</strong></span>
+                    <p class="line-clamp-4 text-lg">울트라부스트는 과연 좋은 러닝화일까? 물론입니다. 트레일 러너부터 풀 마라톤을 준비하는 러너까지, 아디다스 울트라부스트5가 왜 좋은 러닝화인지 그 이유를 알려드리려 합니다.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="css" scoped></style>
---
+++ src/components/XSlideContent.vue
@@ -0,0 +1,345 @@
+<template>
+    <div>
+        <p class="mb-[2rem] mt-[1rem] px-[3rem] text-[30px] font-[550]">
+            열어봐, 너의 🔓 포인트 🅿️
+        </p>
+    </div>
+    <div class="carousel overflow-x-scroll flex pl-[3rem]">
+        <div class="card pr-4 basis-[20%] flex-shrink-0">
+            <picture>
+                <source srcset="/images/Carousel/card1_lg.jpg" media="(min-width: 960px)" width="1050" height="1400">
+                <source srcset="/images/Carousel/card1_md.jpg" media="(min-width: 768px)" width="1050" height="1400">
+                <source srcset="/images/Carousel/card1_sm.jpg" media="(max-width: 767px)" width="1050" height="1400">
+                <img src="/images/Carousel/card1_df.jpg"
+                    alt="kr-adiclubdays-commercial-fw24-launch-clp-commercial_offers-card-01-additional20-asset"
+                    loading="lazy" fetchpriority="auto">
+            </picture>
+            <div class="my-[1rem]">
+                <p class="font-semibold">아디클럽 추가 20% 할인</p>
+            </div>
+            <div class="mt-[1rem] mb-[2rem]">
+                <p class="text-xs">10.4-10.14. 멤버할인 코드 AD20을 장바구니에 입력해보세요.</p>
+            </div>
+            <div>
+                <button class="font-semibold text-sm underline decoration-1">구매하기</button>
+            </div>
+        </div>
+        <div class="card pr-4 basis-[20%] flex-shrink-0">
+            <picture>
+                <source srcset="/images/Carousel/card1_lg.jpg" media="(min-width: 960px)" width="1050" height="1400">
+                <source srcset="/images/Carousel/card1_md.jpg" media="(min-width: 768px)" width="1050" height="1400">
+                <source srcset="/images/Carousel/card1_sm.jpg" media="(max-width: 767px)" width="1050" height="1400">
+                <img src="/images/Carousel/card1_df.jpg"
+                    alt="kr-adiclubdays-commercial-fw24-launch-clp-commercial_offers-card-01-additional20-asset"
+                    loading="lazy" fetchpriority="auto">
+            </picture>
+            <div class="my-[1rem]">
+                <p class="font-semibold">아디클럽 추가 20% 할인</p>
+            </div>
+            <div class="mt-[1rem] mb-[2rem]">
+                <p class="text-xs">10.4-10.14. 멤버할인 코드 AD20을 장바구니에 입력해보세요.</p>
+            </div>
+            <div>
+                <button class="font-semibold text-sm underline decoration-1">구매하기</button>
+            </div>
+        </div>
+        <div class="card pr-4 basis-[20%] flex-shrink-0">
+            <picture>
+                <source srcset="/images/Carousel/card1_lg.jpg" media="(min-width: 960px)" width="1050" height="1400">
+                <source srcset="/images/Carousel/card1_md.jpg" media="(min-width: 768px)" width="1050" height="1400">
+                <source srcset="/images/Carousel/card1_sm.jpg" media="(max-width: 767px)" width="1050" height="1400">
+                <img src="/images/Carousel/card1_df.jpg"
+                    alt="kr-adiclubdays-commercial-fw24-launch-clp-commercial_offers-card-01-additional20-asset"
+                    loading="lazy" fetchpriority="auto">
+            </picture>
+            <div class="my-[1rem]">
+                <p class="font-semibold">아디클럽 추가 20% 할인</p>
+            </div>
+            <div class="mt-[1rem] mb-[2rem]">
+                <p class="text-xs">10.4-10.14. 멤버할인 코드 AD20을 장바구니에 입력해보세요.</p>
+            </div>
+            <div>
+                <button class="font-semibold text-sm underline decoration-1">구매하기</button>
+            </div>
+        </div>
+        <div class="card pr-4 basis-[20%] flex-shrink-0">
+            <picture>
+                <source srcset="/images/Carousel/card1_lg.jpg" media="(min-width: 960px)" width="1050" height="1400">
+                <source srcset="/images/Carousel/card1_md.jpg" media="(min-width: 768px)" width="1050" height="1400">
+                <source srcset="/images/Carousel/card1_sm.jpg" media="(max-width: 767px)" width="1050" height="1400">
+                <img src="/images/Carousel/card1_df.jpg"
+                    alt="kr-adiclubdays-commercial-fw24-launch-clp-commercial_offers-card-01-additional20-asset"
+                    loading="lazy" fetchpriority="auto">
+            </picture>
+            <div class="my-[1rem]">
+                <p class="font-semibold">아디클럽 추가 20% 할인</p>
+            </div>
+            <div class="mt-[1rem] mb-[2rem]">
+                <p class="text-xs">10.4-10.14. 멤버할인 코드 AD20을 장바구니에 입력해보세요.</p>
+            </div>
+            <div>
+                <button class="font-semibold text-sm underline decoration-1">구매하기</button>
+            </div>
+        </div>
+        <div class="card pr-4 basis-[20%] flex-shrink-0">
+            <picture>
+                <source srcset="/images/Carousel/card1_lg.jpg" media="(min-width: 960px)" width="1050" height="1400">
+                <source srcset="/images/Carousel/card1_md.jpg" media="(min-width: 768px)" width="1050" height="1400">
+                <source srcset="/images/Carousel/card1_sm.jpg" media="(max-width: 767px)" width="1050" height="1400">
+                <img src="/images/Carousel/card1_df.jpg"
+                    alt="kr-adiclubdays-commercial-fw24-launch-clp-commercial_offers-card-01-additional20-asset"
+                    loading="lazy" fetchpriority="auto">
+            </picture>
+            <div class="my-[1rem]">
+                <p class="font-semibold">아디클럽 추가 20% 할인</p>
+            </div>
+            <div class="mt-[1rem] mb-[2rem]">
+                <p class="text-xs">10.4-10.14. 멤버할인 코드 AD20을 장바구니에 입력해보세요.</p>
+            </div>
+            <div>
+                <button class="font-semibold text-sm underline decoration-1">구매하기</button>
+            </div>
+        </div>
+        <div class="card pr-4 basis-[20%] flex-shrink-0">
+            <picture>
+                <source srcset="/images/Carousel/card1_lg.jpg" media="(min-width: 960px)" width="1050" height="1400">
+                <source srcset="/images/Carousel/card1_md.jpg" media="(min-width: 768px)" width="1050" height="1400">
+                <source srcset="/images/Carousel/card1_sm.jpg" media="(max-width: 767px)" width="1050" height="1400">
+                <img src="/images/Carousel/card1_df.jpg"
+                    alt="kr-adiclubdays-commercial-fw24-launch-clp-commercial_offers-card-01-additional20-asset"
+                    loading="lazy" fetchpriority="auto">
+            </picture>
+            <div class="my-[1rem]">
+                <p class="font-semibold">아디클럽 추가 20% 할인</p>
+            </div>
+            <div class="mt-[1rem] mb-[2rem]">
+                <p class="text-xs">10.4-10.14. 멤버할인 코드 AD20을 장바구니에 입력해보세요.</p>
+            </div>
+            <div>
+                <button class="font-semibold text-sm underline decoration-1">구매하기</button>
+            </div>
+        </div>
+        <div class="card pr-4 basis-[20%] flex-shrink-0">
+            <picture>
+                <source srcset="/images/Carousel/card1_lg.jpg" media="(min-width: 960px)" width="1050" height="1400">
+                <source srcset="/images/Carousel/card1_md.jpg" media="(min-width: 768px)" width="1050" height="1400">
+                <source srcset="/images/Carousel/card1_sm.jpg" media="(max-width: 767px)" width="1050" height="1400">
+                <img src="/images/Carousel/card1_df.jpg"
+                    alt="kr-adiclubdays-commercial-fw24-launch-clp-commercial_offers-card-01-additional20-asset"
+                    loading="lazy" fetchpriority="auto">
+            </picture>
+            <div class="my-[1rem]">
+                <p class="font-semibold">아디클럽 추가 20% 할인</p>
+            </div>
+            <div class="mt-[1rem] mb-[2rem]">
+                <p class="text-xs">10.4-10.14. 멤버할인 코드 AD20을 장바구니에 입력해보세요.</p>
+            </div>
+            <div>
+                <button class="font-semibold text-sm underline decoration-1">구매하기</button>
+            </div>
+        </div>
+    </div>
+    <div class="carousel overflow-x-scroll flex ml-[3rem] mt-[3rem]">
+        <div class="card flex-shrink-0 mr-[10px] p-3 bg-black border border-black text-center">
+            <p class="text-white font-semibold">추가 20% | 트랙</p>
+        </div>
+        <div class="card flex-shrink-0 mr-[10px] p-3 bg-white border border-black text-center">
+            <p class="text-black font-semibold">추가 20% | 스니커즈</p>
+        </div>
+        <div class="card flex-shrink-0 mr-[10px] p-3 bg-white border border-black text-center">
+            <p class="text-black font-semibold">추가 20% | 저지</p>
+        </div>
+        <div class="card flex-shrink-0 mr-[10px] p-3 bg-white border border-black text-center">
+            <p class="text-black font-semibold">추가 20% | 액세서리</p>
+        </div>
+        <div class="card flex-shrink-0 mr-[10px] p-3 bg-white border border-black text-center">
+            <p class="text-black font-semibold">추가 20% | 아디골프</p>
+        </div>
+        <div class="card flex-shrink-0 mr-[10px] p-3 bg-white border border-black text-center">
+            <p class="text-black font-semibold">추가 20% | 아울렛</p>
+        </div>
+    </div>
+    <div ref="carousel" @scroll="updateScrollbar($event)" class="carousel overflow-x-scroll flex ml-[3rem] mt-[1rem]">
+        <div class="card pr-4 basis-[20%] flex-shrink-0">
+            <picture class="relative">
+                <div class="absolute m-3 top-0 right-0 cursor-pointer">
+                    <IconWishList class="w-6 h-6" />
+                </div>
+                <div class="absolute bg-white bottom-0 ml-1 p-1">
+                    <p>75,000 원</p>
+                </div>
+                <img src="/images/Carousel/jacket.avif" loading="lazy" fetchpriority="auto">
+            </picture>
+            <div class="my-[1rem]">
+                <p class="">에센셜 3S 트리콧 트랙 재킷</p>
+            </div>
+            <div class="mt-[1rem] mb-[2rem]">
+                <p class="text-slate-400">Sportswear</p>
+            </div>
+        </div>
+        <div class="card pr-4 basis-[20%] flex-shrink-0">
+            <picture class="relative">
+                <div class="absolute m-3 top-0 right-0 cursor-pointer">
+                    <IconWishList class="w-6 h-6" />
+                </div>
+                <div class="absolute bg-white bottom-0 ml-1 p-1">
+                    <p>75,000 원</p>
+                </div>
+                <img src="/images/Carousel/jacket.avif" loading="lazy" fetchpriority="auto">
+            </picture>
+            <div class="my-[1rem]">
+                <p class="">에센셜 3S 트리콧 트랙 재킷</p>
+            </div>
+            <div class="mt-[1rem] mb-[2rem]">
+                <p class="text-slate-400">Sportswear</p>
+            </div>
+        </div>
+        <div class="card pr-4 basis-[20%] flex-shrink-0">
+            <picture class="relative">
+                <div class="absolute m-3 top-0 right-0 cursor-pointer">
+                    <IconWishList class="w-6 h-6" />
+                </div>
+                <div class="absolute bg-white bottom-0 ml-1 p-1">
+                    <p>75,000 원</p>
+                </div>
+                <img src="/images/Carousel/jacket.avif" loading="lazy" fetchpriority="auto">
+            </picture>
+            <div class="my-[1rem]">
+                <p class="">에센셜 3S 트리콧 트랙 재킷</p>
+            </div>
+            <div class="mt-[1rem] mb-[2rem]">
+                <p class="text-slate-400">Sportswear</p>
+            </div>
+        </div>
+        <div class="card pr-4 basis-[20%] flex-shrink-0">
+            <picture class="relative">
+                <div class="absolute m-3 top-0 right-0 cursor-pointer">
+                    <IconWishList class="w-6 h-6" />
+                </div>
+                <div class="absolute bg-white bottom-0 ml-1 p-1">
+                    <p>75,000 원</p>
+                </div>
+                <img src="/images/Carousel/jacket.avif" loading="lazy" fetchpriority="auto">
+            </picture>
+            <div class="my-[1rem]">
+                <p class="">에센셜 3S 트리콧 트랙 재킷</p>
+            </div>
+            <div class="mt-[1rem] mb-[2rem]">
+                <p class="text-slate-400">Sportswear</p>
+            </div>
+        </div>
+        <div class="card pr-4 basis-[20%] flex-shrink-0">
+            <picture class="relative">
+                <div class="absolute m-3 top-0 right-0 cursor-pointer">
+                    <IconWishList class="w-6 h-6" />
+                </div>
+                <div class="absolute bg-white bottom-0 ml-1 p-1">
+                    <p>75,000 원</p>
+                </div>
+                <img src="/images/Carousel/jacket.avif" loading="lazy" fetchpriority="auto">
+            </picture>
+            <div class="my-[1rem]">
+                <p class="">에센셜 3S 트리콧 트랙 재킷</p>
+            </div>
+            <div class="mt-[1rem] mb-[2rem]">
+                <p class="text-slate-400">Sportswear</p>
+            </div>
+        </div>
+        <div class="card pr-4 basis-[20%] flex-shrink-0">
+            <picture class="relative">
+                <div class="absolute m-3 top-0 right-0 cursor-pointer">
+                    <IconWishList class="w-6 h-6" />
+                </div>
+                <div class="absolute bg-white bottom-0 ml-1 p-1">
+                    <p>75,000 원</p>
+                </div>
+                <img src="/images/Carousel/jacket.avif" loading="lazy" fetchpriority="auto">
+            </picture>
+            <div class="my-[1rem]">
+                <p class="">에센셜 3S 트리콧 트랙 재킷</p>
+            </div>
+            <div class="mt-[1rem] mb-[2rem]">
+                <p class="text-slate-400">Sportswear</p>
+            </div>
+        </div>
+        <div class="card pr-4 basis-[20%] flex-shrink-0">
+            <picture class="relative">
+                <div class="absolute m-3 top-0 right-0 cursor-pointer">
+                    <IconWishList class="w-6 h-6" />
+                </div>
+                <div class="absolute bg-white bottom-0 ml-1 p-1">
+                    <p>75,000 원</p>
+                </div>
+                <img src="/images/Carousel/jacket.avif" loading="lazy" fetchpriority="auto">
+            </picture>
+            <div class="my-[1rem]">
+                <p class="">에센셜 3S 트리콧 트랙 재킷</p>
+            </div>
+            <div class="mt-[1rem] mb-[2rem]">
+                <p class="text-slate-400">Sportswear</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Custom Scrollbar -->
+    <div class="px-[3rem]">
+        <!-- role="scrollbar" tabindex="0" :aria-controls="'carousel'" aria-orientation="horizontal" aria-valuemin="0"
+            :aria-valuemax="100" :aria-valuenow="scrollPosition" aria-hidden="false" -->
+        <div 
+            class="scrollbar-wrapper w-full h-1 bg-gray-200">
+            <span class="scrollbar relative h-full block">
+                <span ref="scrollbarTrack" class="scrollbar-track absolute h-full bg-black w-1/3"
+                    :style="{ left: `${scrollbarLeft}px` }"></span>
+            </span>
+        </div>
+    </div>
+
+    <!-- Custom Scrollbar -->
+    <div class="px-[3rem]">
+        <!-- role="scrollbar" tabindex="0" :aria-controls="'carousel'" aria-orientation="horizontal" aria-valuemin="0"
+            :aria-valuemax="100" :aria-valuenow="scrollPosition" aria-hidden="false" -->
+        <div 
+            class="scrollbar-wrapper w-full h-1 bg-gray-200">
+            <span class="scrollbar relative h-full block">
+                <span ref="scrollbarTrack2" class="scrollbar-track absolute h-full bg-black w-1/3"
+                    :style="{ left: `${scrollbarLeft}px` }"></span>
+            </span>
+        </div>
+    </div>
+
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import IconWishList from './icons/IconWishList.vue';
+
+const scrollbarLeft = ref(null);
+// const scrollPosition = ref(null);
+const carousel = ref(null);
+const scrollbarTrack = ref(null);
+const scrollbarTrack2 = ref(null);
+// 스크롤바
+const updateScrollbar = (event, num) => {
+    const contentWidth = event.target.scrollWidth - event.target.clientWidth;
+    const scrollbarWidth = scrollbarTrack.value.parentElement.clientWidth - scrollbarTrack.value.clientWidth;
+    const scrollRatio = event.target.scrollLeft / contentWidth;
+    scrollbarLeft.value = scrollRatio * scrollbarWidth;
+    // Update aria-valuenow
+    // scrollPosition.value  = Math.round((carousel.value.scrollLeft / contentWidth) * 100);
+}
+</script>
+
+<style scoped>
+.carousel {
+    -ms-overflow-style: none;
+    /* IE and Edge */
+    scrollbar-width: none;
+    /* Firefox */
+}
+
+.carousel::-webkit-scrollbar {
+    display: none;
+    /* Chrome, Safari, Opera*/
+}
+</style>
