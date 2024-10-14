0eee33239dc21c68852eaf93f4f5ad5565a92f91
HomeDesktop <nr8122@gmail.com>
2024-10-14 23:10:40 +0900

    블로그 섹션, 히스토리 섹션 만드는 중...

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -2,7 +2,9 @@
     <Header :notification="''"></Header>
     <Banner/>
     <XSlideContent/>
-    
+    <XSlideContent/>
+    <BlogSection/>
+    <HistorySection/>
 </template>
 
 <script setup>
@@ -10,6 +12,8 @@ import { ref } from 'vue';
 import Banner from '@/components/Banner.vue';
 import Header from '@/components/Header.vue';
 import XSlideContent from '@/components/XSlideContent.vue';
+import BlogSection from '@/components/BlogSection.vue';
+import HistorySection from '@/components/HistorySection.vue';
 </script>
 
 <style scoped></style>
